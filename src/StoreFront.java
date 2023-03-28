fe34a7fee87c2c28cd6373e214545e1736730469
Salvatore2003 <bslamarca@gmail.com>
2023-03-28 17:14:07 -0400

    createAccount

--- src/StoreFront.java
+++ src/StoreFront.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class StoreFront {
     public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
         System.out.println("Hello world!");
+    }
+    public static boolean createAccount () {
+
     }
 }
