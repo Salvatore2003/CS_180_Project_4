d85bd6484fe2b579c73fe708b88bd098cf9d83db
dhruvw <wadhwa19@purdue.edu>
2023-04-07 10:47:25 -0400

    initial seller

---
+++ src/Product.java
@@ -0,0 +1,26 @@
+public class Product {
+    String productName;
+    String storeName;
+    String description;
+    int quantityAvailable;
+    double price;
+    int quantitySold;
+
+    public Product(String productName, String storeName, String description, int quantityAvailable, double price){
+        this.productName = productName;
+        this.storeName = storeName;
+        this.description = description;
+        this.quantityAvailable = quantityAvailable;
+        this.price = price;
+        quantitySold = 0;
+    }
+
+    public Product(String productName, String storeName, String description, int quantityAvailable, double price, int quantitySold){
+        this.productName = productName;
+        this.storeName = storeName;
+        this.description = description;
+        this.quantityAvailable = quantityAvailable;
+        this.price = price;
+        this.quantitySold = quantitySold;
+    }
+}
