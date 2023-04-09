2cff41eb669b416ac39e357ee04a1ecd0f017a0a
Rakshit-p <75575332+Rakshit-p@users.noreply.github.com>
2023-04-09 16:53:53 -0400

    Update MarketPlaceTest.java

--- MarketPlaceTest.java
+++ MarketPlaceTest.java
@@ -7,31 +7,5 @@ import static org.junit.jupiter.api.Assertions.*;
 class MarketPlaceTest {
     private MarketPlace marketPlace;
 
-    @BeforeEach
-    void setUp() {
-        String user = "testUser";
-        String email = "testUser@example.com";
-        Scanner scan = new Scanner(System.in);
-        marketPlace = new MarketPlace(user, email, scan);
-    }
-
-    @Test
-    void createAndDeleteStoreFile() {
-        String user = "testUser";
-        String storeName = "testStore";
-        String fileName = user + "_" + storeName;
-
-        // Test file creation
-        assertTrue(marketPlace.createFile(user, storeName));
-
-        // Check if the file exists
-        File file = new File(fileName);
-        assertTrue(file.exists());
-
-        // Test file deletion
-        assertTrue(marketPlace.deleteStore(fileName));
-
-        // Check if the file was deleted
-        assertFalse(file.exists());
-    }
+    
 }
