11f1a4479e2a31cad575bbaeee6f6cd380c3ab64
Rakshit-p <75575332+Rakshit-p@users.noreply.github.com>
2023-04-09 21:21:53 -0400

    Update UserMessagesTest.java

--- src/UserMessagesTest.java
+++ src/UserMessagesTest.java
@@ -1,77 +1,58 @@
-/**import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public class UserMessagesTest {
-    private User user1;
-    private User user2;
-    private ArrayList<User> users;
-    private Scanner scan;
-    private UserMessages userMessages;
-    
-    @Before
-    public void setUp() {
-        user1 = new User("Alice");
-        user2 = new User("Bob");
-        users = new ArrayList<>();
+
+    @Test
+    public void testUserMessages() {
+        User user1 = new User("testUser1", "testPassword1", "testEmail1@example.com", true, false);
+        User user2 = new User("testUser2", "testPassword2", "testEmail2@example.com", true, false);
+        ArrayList<User> users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
-        scan = new Scanner(System.in);
-        userMessages = new UserMessages(user1, users, scan);
-    }
-    
-    @Test
-    public void testRunMessages() {
-        // Test that user can exit
-        String input = "5\n";
-        System.setIn(input.getBytes());
-        userMessages.runMessages();
-        assertEquals("5", input.trim());
-        
-        // Test that user can message a user
-        input = "1\nBob\nHello Bob!\n";
-        System.setIn(input.getBytes());
-        userMessages.runMessages();
-        assertEquals("1", input.split("\n")[0]);
-        assertEquals("Bob", input.split("\n")[1]);
-        assertEquals("Hello Bob!", input.split("\n")[2]);
-        
-        // Test that user can read a message
-        input = "2\n";
-        System.setIn(input.getBytes());
-        userMessages.runMessages();
-        assertEquals("2", input.trim());
-        
-        // Test that user can edit a message
-        input = "3\n1\nNew message\n";
-        System.setIn(input.getBytes());
-        userMessages.runMessages();
-        assertEquals("3", input.split("\n")[0]);
-        assertEquals("1", input.split("\n")[1]);
-        assertEquals("New message", input.split("\n")[2]);
-        
-        // Test that user can delete a message
-        input = "4\n1\n";
-        System.setIn(input.getBytes());
-        userMessages.runMessages();
-        assertEquals("4", input.split("\n")[0]);
-        assertEquals("1", input.split("\n")[1]);
-    }
-    
-    @Test
-    public void testMessageUser() {
-        // Test that message is sent to the correct user
-        String message = "Hello Bob!";
-        UserMessages.messageUser(user1, user2, message);
+
+        // Test case for messageUser
+        UserMessages userMessages1 = new UserMessages(user1, users, new MockScanner("1\ntestUser2\nHello there\n5\n"));
+        userMessages1.runMessages();
         assertEquals(1, user2.getMessages().size());
-        assertEquals(message, user2.getMessages().get(0).getMessage());
-        
-        // Test that message is added to existing messages for user
-        message = "How are you?";
-        UserMessages.messageUser(user2, user1, message);
-        assertEquals(2, user1.getMessages().size());
-        assertEquals(message, user1.getMessages().get(1).getMessage());
+        assertEquals("Hello there", user2.getMessages().get(0).getContent());
+
+        // Test case for readMessage
+        UserMessages userMessages2 = new UserMessages(user2, users, new MockScanner("2\n1\n5\n"));
+        userMessages2.runMessages();
+        assertTrue(user2.getMessages().get(0).isRead());
+
+        // Test case for editMessage
+        UserMessages userMessages3 = new UserMessages(user1, users, new MockScanner("3\n1\nUpdated message\n5\n"));
+        userMessages3.runMessages();
+        assertEquals("Updated message", user2.getMessages().get(0).getContent());
+
+        // Test case for deleteMessage
+        UserMessages userMessages4 = new UserMessages(user2, users, new MockScanner("4\n1\n5\n"));
+        userMessages4.runMessages();
+        assertEquals(0, user2.getMessages().size());
     }
-}*/
+
+    static class MockScanner extends Scanner {
+        private final String[] inputs;
+        private int currentIndex = 0;
+
+        public MockScanner(String input) {
+            super((InputStream) null);
+            inputs = input.split("\n");
+        }
+
+        @Override
+        public String nextLine() {
+            if (currentIndex < inputs.length) {
+                return inputs[currentIndex++];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+}
