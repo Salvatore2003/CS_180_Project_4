a3cf7fbb8f9701dff6db8f2dcc9e986f78e22baa
njclarks <129313125+njclarks@users.noreply.github.com>
2023-04-10 13:29:00 -0400

    Update Messages.java

--- src/Messages.java
+++ src/Messages.java
@@ -1,13 +1,19 @@
-//Nick made this class and worked on it. He does not know how to push to GitHub so I did it for him
-
 import java.util.*;
-public class Messages{
-    ArrayList<Message> inbox;
-    public Messages(ArrayList<Message> inbox) {
-        this.inbox = inbox;
-    }
 
-    public void printInbox() {
+/**
+ * Utility class containing all the methods to add, remove, and edit messages as well as a
+ * method that prints the inbox and outbox of the user this class is assigned to.
+ */
+public class Messages{
+    public Messages() {
+    }
+    
+    /**
+     * Prints the subjects of the emails in a user's inbox, or prints
+     * "Empty Inbox!" if the user's inbox contains no mail
+     */
+    public void printInbox(User user) {
+        ArrayList<Message> inbox = user.getInbox();
         if(inbox.size() == 0) {
             System.out.println("Empty Inbox!");
             return;
@@ -17,7 +23,24 @@ public class Messages{
             System.out.println(inbox.get(i).getSubject());
         }
     }
-
+    
+    public void printOutbox(User user) {
+        ArrayList<Message> outbox = user.getOutbox();
+        if(outbox.size() == 0) {
+            System.out.println("Empty Inbox!");
+            return;
+        }
+        System.out.println("You have " + outbox.size() + " messages.");
+        for(int i = 0;i < outbox.size();i++) {
+            System.out.println(outbox.get(i).getSubject());
+        }
+    }
+    
+    /**
+     * Sends a message to a recipient's outbox containing a subject and a message, 
+     * then adds the same message to the sender's outbox. Does not allow buyers
+     * to message buyers or sellers to message sellers.
+     */
     public void sendMessage(String subject, String message, User sender, User recipient) {
         if(sender.isBuyer() && recipient.isBuyer()) {
             System.out.println("You cannot message another buyer!");
@@ -27,17 +50,20 @@ public class Messages{
             System.out.println("You cannot message another seller!");
             return;
         }
-
+        
         try {
             ArrayList<Message> recInbox = recipient.getInbox();
             recInbox.add(new Message(subject, message, sender, recipient));
             recipient.setInbox(recInbox);
-            System.out.println("Message sent successfully.");
+            System.out.println("Message sent successfully.");            
         } catch (Exception e) {
             System.out.println("Error sending message.");
         }
     }
-
+    
+    /**
+     * Edits a message in both the sender's outbox and the recipient's inbox
+     */
     public void editMessage(Message oldMessage, String newSubject, String newMessage, User sender, User recipient) {
         try {
             ArrayList<Message> si = sender.getOutbox();
@@ -66,7 +92,11 @@ public class Messages{
             return;
         }
     }
-
+    
+    /**
+     * Deletes a message from the sender's outbox while not deleting it from
+     * the recipient's inbox
+     */
     public void deleteMessage(Message delMessage, User sender) {
         ArrayList<Message> si = sender.getOutbox();
         for(int i = 0;i < si.size();i++) {
